56a6638751f8f18bf6b74d772f49352e932c47a9
Morgan Haskins <morgan.haskins@fusionary.com>
2021-04-01 21:56:19 -0400

    Progress through step 6

--- StudentSubmissionsLab3.js
+++ StudentSubmissionsLab3.js
@@ -25,6 +25,78 @@ const submissions = [
     }
 ];
 
+// for(let submission of submissions){
+//     console.log(`Name: ${submission.name}, Score: ${submission.score}, Date: ${submission.date}, Passed: ${submission.passed}`
+//     );
+// }
+
 function addSubmission(array, newName, newScore, newDate){
+    let newStudentSubmission =
+        {
+            name: newName,
+            score: newScore,
+            date: newDate,
+            passed: true,
+        };
+
+    array.push(newStudentSubmission);
+    console.log(submissions);
+
+    if (newStudentSubmission.score >= 60){
+        newStudentSubmission.passed = true;
+    }else{
+        newStudentSubmission.passed = false;
+    }
     
 }
+//addSubmission(submissions, 'Morgan', 50, '2021-03-25');
+
+function deleteSubmissionByIndex(array, index){
+    array.splice(index, 1);
+    console.log(submissions);
+}
+//deleteSubmissionByIndex(submissions, 4);
+
+
+function deleteSubmissionByName(array, name){
+    const index = array.findIndex(submission => submission.name === name);
+    array.splice(index, 1);
+    console.log(submissions)
+}
+//deleteSubmissionByName(submissions, 'Joe');
+
+function editSubmission(array, index, score){
+    array[index].score = score;
+    if(array[index].score >= 60){
+        array[index].passed = true;
+    }else{
+        array[index].passed = false;
+    }
+}
+//editSubmission(submissions, 1, 75);
+//console.log(submissions);
+
+function findSubmissionByName(array, name){
+    // return array.name === name;
+    // console.log(submissions.find());
+    const index = array.find(submission => submission.name === name);
+    console.log(submissions.find());
+}
+
+function findLowestScore(array){
+
+}
+
+function findAverageScore(array){
+
+}
+
+function filterPassing(array){
+
+}
+
+function filter90AndAbove(array){
+    
+}
+
+
